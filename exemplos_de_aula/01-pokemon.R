f7b3b03d4fbc7b6bc1c29c36d98134c5429b02c3
Julio Trecenti <julio.trecenti@gmail.com>
2020-11-09 22:15:35 -0300

    exemplos de aula, modificados

--- exemplos_de_aula/01-pokemon.R
+++ exemplos_de_aula/01-pokemon.R
@@ -14,30 +14,44 @@ content(r_pokemon, "text")
 content(r_pokemon, "raw")
 content(r_pokemon, "parsed")
 
-content(r_pokemon, "parsed", simplifyDataFrame = TRUE)
+r_pokemon %>%
+  content("parsed", simplifyDataFrame = TRUE)
 
 fromJSON(content(r_pokemon, "text"))
+
+content(r_pokemon, "text") %>%
+  fromJSON()
 
 # 2. Pegar informações filtrando com o código
 
 q_pokemon <- list(
-  limit = 8, 
+  limit = 8,
   offset = 1
 )
 
 r_pokemon_filtrado <- GET(
-  paste0(u_base, "/pokemon"), 
+  paste0(u_base, "/pokemon"),
   query = q_pokemon
 )
 
 content(r_pokemon_filtrado, simplifyDataFrame = TRUE)
+
+
+
+r_pokemon_filtrado2 <- GET(
+  "https://pokeapi.co/api/v2/pokemon?limit=8&offset=1"
+)
+
+content(r_pokemon_filtrado2, simplifyDataFrame = TRUE)
+
 
 # 3. O que faz httr::write_disk()?
 
 dir.create("output", showWarnings = FALSE, recursive = TRUE)
 
 GET(
-  paste0(u_base, "/pokemon"), 
-  query = q_pokemon, 
-  write_disk("output/01-pokemon.json")
+  paste0(u_base, "/pokemon"),
+  query = q_pokemon,
+  write_disk("output/01-pokemon.json", overwrite = TRUE)
 )
+
--- exemplos_de_aula/02-sabesp.R
+++ exemplos_de_aula/02-sabesp.R
@@ -1,7 +1,8 @@
 # 1. colete os dados de mananciais da sabesp
-u_sabesp <- "http://mananciais.sabesp.com.br/api/Mananciais/ResumoSistemas/2020-03-15"
+u_sabesp <- "http://mananciais.sabesp.com.br/api/Mananciais/ResumoSistemas/2020-11-04"
 r_sabesp <- httr::GET(u_sabesp)
 results <- httr::content(r_sabesp, simplifyDataFrame = TRUE)
+
 results$ReturnObj$sistemas
 
 
