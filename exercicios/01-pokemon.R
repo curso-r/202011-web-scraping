91df5ba5afab60e74345b85831ff60221f822578
Julio Trecenti <julio.trecenti@gmail.com>
2020-11-11 20:35:05 -0300

    exercicios resolvidos

--- exercicios/01-pokemon.R
+++ exercicios/01-pokemon.R
@@ -7,10 +7,40 @@ u_base <- "https://pokeapi.co/api/v2/"
 # 1. Acesse todos os resultados de "type"
 # Dica: qual é o endpoint que devemos utilizar?
 
+u_type <- paste0(u_base, "type")
+r_type <- httr::GET(u_type, query = list(limit = 12))
+
 # 2. Encontre o link do type "grass" e guarde em um objeto.
 # Dica: você pode fazer isso manualmente ou com um código em R, usando {purrr}
 
+httr::content(r_type)$results %>%
+  purrr::keep(~.x$name == "grass") %>%
+  purrr::pluck(1, "url")
+
+u_grass <- httr::content(r_type, simplifyDataFrame = TRUE)$results %>%
+  dplyr::filter(name == "grass") %>%
+  dplyr::pull(url)
+
 # 3. Crie um data.frame com os 20 primeiros pokemons do tipo "grass"
-# Dica: utilize o parâmetro query=.
-# Além disso, tabelas ficam mais fáceis de visualizar quando rodamos 
+# Além disso, tabelas ficam mais fáceis de visualizar quando rodamos
 # tibble::as_tibble(tab)
+
+r_grass <- httr::GET(u_grass)
+
+tab_pokemon <- httr::content(r_grass, "text") %>%
+  jsonlite::fromJSON() %>%
+  purrr::pluck("pokemon") %>%
+  head(20) %>%
+  tibble::as_tibble()
+
+# lista[[1]]
+# lista %>% purrr::pluck(1)
+
+# lista$elemento
+# lista %>% purrr::pluck("elemento")
+
+# lista$elemento$outro_elemento
+# lista %>% purrr::pluck("elemento", "outro_elemento")
+
+# lista$elemento[[3]]
+# lista %>% purrr::pluck("elemento", 3)
--- exercicios/02-sabesp.R
+++ exercicios/02-sabesp.R
@@ -1,13 +1,20 @@
 # 1. Escreva uma função que recebe uma data e retorna a tabela dos mananciais
 
 baixar_sabesp <- function(data) {
-  # ...
-  # Dica: u_base <- "http://mananciais.sabesp.com.br/api/Mananciais/ResumoSistemas/"
+  u_base <- "http://mananciais.sabesp.com.br/api/Mananciais/ResumoSistemas/"
+  u_dia <- paste0(u_base, data)
+  r_dia <- httr::GET(u_dia)
+  httr::content(r_dia, "text") %>%
+    jsonlite::fromJSON() %>%
+    purrr::pluck("ReturnObj", "sistemas") %>%
+    tibble::as_tibble() %>%
+    # limpar nomes
+    janitor::clean_names()
 }
 
 # 2. Armazene no objeto tab_sabesp a tabela do dia `Sys.Date() - 1` (ontem)
 
-tab_sabesp <- "..."
+tab_sabesp <- baixar_sabesp(Sys.Date() - 1)
 
 # 3. [extra] Arrume os dados para que fique assim:
 
@@ -15,3 +22,9 @@ tab_sabesp <- "..."
 # Variables: 2
 # $ nome   <fct> Cantareira, Alto Tietê, Guarapiranga, Cotia, Rio Grande, Rio Claro, São Lourenço
 # $ volume <dbl> 63.25681, 90.35307, 84.25839, 102.28429, 93.66445, 99.85615, 97.33682
+
+tab_sabesp %>%
+  dplyr::transmute(
+    nome = factor(nome),
+    volume = volume_porcentagem
+  )
