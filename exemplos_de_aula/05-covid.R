9d45358ba4a3d4445ea7a688a738efa899281e7b
Julio Trecenti <julio.trecenti@gmail.com>
2020-11-11 22:26:07 -0300

    exemplos atualizados

--- exemplos_de_aula/05-covid.R
+++ exemplos_de_aula/05-covid.R
@@ -3,4 +3,67 @@ library(httr)
 
 u_msaude <- "https://covid.saude.gov.br"
 
+httr::GET(
+  u_msaude,
+  httr::write_disk("pagina_msaude.html")
+)
+
+
 # (live)
+
+u_portal_geral <- "https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeral"
+
+r <- httr::GET(
+  u_portal_geral,
+  httr::add_headers(
+    "x-parse-application-id" = "unAFkcaNDeXajurGB7LChj8SgQYS2ptm"
+  )
+)
+
+link_csv <- httr::content(r) %>%
+  purrr::pluck("results", 1, "arquivo", "url")
+
+httr::GET(
+  link_csv,
+  httr::write_disk("dados_covid.csv"),
+  httr::progress()
+)
+
+
+# grafico -----------------------------------------------------------------
+
+da <- vroom::vroom("dados_covid.csv")
+
+library(ggplot2)
+
+# versao incompleta
+da %>%
+  janitor::clean_names() %>%
+  dplyr::filter(
+    regiao != "Brasil",
+    !is.na(estado),
+    is.na(municipio)
+  ) %>%
+  dplyr::arrange(data) %>%
+  dplyr::group_by(regiao, estado, semana_epi) %>%
+  dplyr::summarise(
+    obitos_acumulado = dplyr::last(obitos_acumulado),
+    pop = dplyr::last(populacao_tcu2019),
+    .groups = "drop"
+  ) %>%
+  dplyr::mutate(taxa = obitos_acumulado / pop) %>%
+  ggplot(aes(x = semana_epi, y = taxa,
+             colour = estado)) +
+  geom_step() +
+  facet_wrap(~regiao)
+
+
+
+
+
+
+
+
+
+
+
--- exemplos_de_aula/06-html.R
+++ exemplos_de_aula/06-html.R
@@ -1,14 +1,14 @@
 library(xml2)
 
 # ler o HTML
-html <- read_html("exemplos_de_aula/html_exemplo.html")
+html <- xml2::read_html("exemplos_de_aula/html_exemplo.html")
 
 # Coletar todos os nodes com a tag <p>
 nodes <- xml_find_all(html, "//p")
 nodes
 
 # buscar no documento todo ou dentro das tags
-xml_find_all(nodes, "//head")
+xml_find_all(nodes, ".//head")
 xml_find_all(nodes, "/head")
 
 # Extrair o texto contido em cada um dos nodes
