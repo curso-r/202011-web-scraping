59d5329fb9b00f91327803daead32865f8e3faa0
C. Lente <7017340+clente@users.noreply.github.com>
2020-11-16 18:33:52 -0300

    Minor fix

--- exemplos_de_aula/07-chancedegol.R
+++ exemplos_de_aula/07-chancedegol.R
@@ -4,15 +4,14 @@ library(rvest)
 
 u_cdg <- "http://www.chancedegol.com.br/br19.htm"
 
-cdg_html <- u_cdg %>%
-  GET() %>%
-  content('text', encoding = 'latin1')
+cdg_html <- GET(u_cdg)
 
 cdg_table <- cdg_html  %>%
   read_html() %>%
-  xml_find_first('//table')
+  xml_find_first('//table') %>%
+  html_table()
 
 cores <- cdg_html %>%
+  read_html() %>%
   xml_find_all('//font[@color="#FF0000"]') %>%
   xml_text()
-
--- exemplos_de_aula/08-tjsp.R
+++ exemplos_de_aula/08-tjsp.R
@@ -5,7 +5,7 @@ library(rvest)
 u_tjsp <- "https://esaj.tjsp.jus.br/cjsg/resultadoCompleta.do"
 
 b_tjsp <- list(
-  dados.buscaInteiroTeor = "coronavirus", 
+  dados.buscaInteiroTeor = "coronavirus",
   dados.origensSelecionadas = "T"
 )
 
@@ -18,15 +18,11 @@ u_tjsp_pag <- "https://esaj.tjsp.jus.br/cjsg/trocaDePagina.do"
 q_tjsp_pag <- list(tipoDeDecisao = "A", pagina = 1, conversationId = "")
 r_tjsp_pag <- GET(u_tjsp_pag, query = q_tjsp_pag)
 
-tab_html <- r_tjsp_pag %>% 
-  read_html(encoding = "UTF-8") %>% 
+tab_html <- r_tjsp_pag %>%
+  read_html(encoding = "UTF-8") %>%
   xml_find_all("//tr[@class='fundocinza1']//table")
 
-tab_html[[1]] %>% 
-  html_table(fill = TRUE) %>% 
-  as_tibble() %>% 
+tab_html[[1]] %>%
+  html_table(fill = TRUE) %>%
+  as_tibble() %>%
   mutate(X1 = str_squish(X1))
-
-
-
-
