7011da96e2eaa4c73fe034ae60c428da3eefe27a
Julio Trecenti <julio.trecenti@gmail.com>
2020-11-16 22:03:17 -0300

    correcao exercicio html

--- exercicios/06-html.R
+++ exercicios/06-html.R
@@ -5,6 +5,19 @@ html <- read_html("exemplos_de_aula/html_exemplo.html")
 
 # 1. Qual a diferença entre xml_find_all() e xml_find_first()?
 
+# esses dois são equivalentes
+xml2::xml_find_all(html, "//body")[[1]]
+xml2::xml_find_first(html, "//body")
+
+# esses dois não são
+xml2::xml_find_all(html, "//body")
+xml2::xml_find_first(html, "//body")
+
 # 2. O que faz a função contains() aplicada dentro do XPath?
 ## Dica: xml_find_all(html, "//p[contains(@style, 'blue')]")
 
+xml_find_all(html, "//p[contains(@style, 'blue')]")
+
+xml_find_all(html, "//p[contains(text(), 'Sou')]")
+
+# é como str_detect(), mas para xpath
