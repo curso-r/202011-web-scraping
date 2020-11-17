257b8236e3fb82d7b0b2550ad6bf1da1c77d3fa3
Julio Trecenti <julio.trecenti@gmail.com>
2020-11-16 22:03:10 -0300

    exemplo tjsp modificado

--- exemplos_de_aula/08-tjsp.R
+++ exemplos_de_aula/08-tjsp.R
@@ -9,6 +9,8 @@ b_tjsp <- list(
   dados.origensSelecionadas = "T"
 )
 
+# Dica: abjutils::chrome_to_body()
+
 r_tjsp <- POST(u_tjsp, body = b_tjsp)
 
 # muitas vezes precisa disso aqui
@@ -18,11 +20,25 @@ u_tjsp_pag <- "https://esaj.tjsp.jus.br/cjsg/trocaDePagina.do"
 q_tjsp_pag <- list(tipoDeDecisao = "A", pagina = 1, conversationId = "")
 r_tjsp_pag <- GET(u_tjsp_pag, query = q_tjsp_pag)
 
+scrapr::html_view(r_tjsp_pag)
+
 tab_html <- r_tjsp_pag %>%
   read_html(encoding = "UTF-8") %>%
+  ## alternativa
+  # xml_find_all("//table//tr")
   xml_find_all("//tr[@class='fundocinza1']//table")
 
 tab_html[[1]] %>%
   html_table(fill = TRUE) %>%
   as_tibble() %>%
   mutate(X1 = str_squish(X1))
+
+# o poder do str_squish()
+tab_html[[1]] %>%
+  html_table(fill = TRUE) %>%
+  as_tibble() %>%
+  mutate(X1 = str_squish(X1)) %>%
+  with(X1[2]) %>%
+  cat()
+
+
